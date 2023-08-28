558de1895ed64fc72ad84c07ab5ff1fadf98aa9e
Olena Zdanovska <freshjs@gmail.com>
2023-08-28 10:23:32 +0300

    Example JSX

--- src/App.js
+++ src/App.js
@@ -2,54 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const value = 45;
+const array = [1, 2, 3, 4, 5];
+function ff() {
+  return 'text from function';
+}
+
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  return React.createElement(
-    'div',
-    { className: 'App' },
-    React.createElement(
-      'header',
-      { className: 'App-header' },
-      React.createElement('img', {
-        src: logo,
-        className: 'App-logo',
-        alt: 'logo',
-      }),
-      React.createElement(
-        'p',
-        null,
-        'Edit ',
-        React.createElement('code', null, 'src/App.js'),
-        ' and save to reload.'
-      ),
-      React.createElement(
-        'a',
-        {
-          className: 'App-link',
-          href: 'https://reactjs.org',
-          target: '_blank',
-          rel: 'noopener noreferrer',
-        },
-        'Learn React'
-      )
-    )
+  return (
+    <>
+      <h1>
+        Hi <em>JSX!</em> value = {value}
+      </h1>
+      <p>{Math.random() > 0.5 ? 'yes' : 'no'} {ff()}</p>
+      <ul>
+        {array.map((num) => (
+          <li>{num}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
